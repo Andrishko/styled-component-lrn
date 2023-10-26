f2dd436a7ca8d8a63f6e6a23da56dd5ed5ea2ef4
Andrishko <andrewpetrovckiy@gmail.com>
2023-10-26 15:42:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { H1 } from './components/Title';
+import { DefaultButton, ExtendedButton } from './components/Buttons';
+import styled from 'styled-components'
+
+
+const Wrapper = styled.div`
+h1{
+  text-align: center;
+  color: violet;
+}
+
+p{
+  font-size: 40px;
+}
+
+button{
+  background-color: pink;
+  padding: 4px 8px;
+  border: none;  
+}
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <div>
+        <H1>Styled Components</H1>
+        <p>Cillum culpa deserunt enim et eiusmod quis proident consequat tempor ipsum sunt esse.</p>
+        <DefaultButton>Click ME!</DefaultButton>
+        <DefaultButton red>Click ME!</DefaultButton>
+        <ExtendedButton red>Click ME!</ExtendedButton>
+      </div>
+    </Wrapper>
   );
 }
 
